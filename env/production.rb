fa344751c920e5c76747ae635e2f546d1a42caf6
Sam Smoot <ssmoot@gmail.com>
2012-08-09 16:20:41 -0500

    Compile assets in production.

--- env/production.rb
+++ env/production.rb
@@ -1,4 +1,6 @@
 # Make use of Tilt (https://github.com/rtomayko/tilt) cache for compiled templates
 Harbor::View.cache_templates!
 
+config.assets.compile = true
+
 config.couchdb = "http://#{File.read(".couchdb").strip}@ssmoot.cloudant.com:5984/blog"
