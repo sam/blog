20f96754286458c11985de0efcf5b01143453e4e
Sam Smoot <ssmoot@gmail.com>
2012-08-09 15:38:11 -0500

    Fixed couchdb port number in production.rb.

--- env/production.rb
+++ env/production.rb
@@ -1,4 +1,4 @@
 # Make use of Tilt (https://github.com/rtomayko/tilt) cache for compiled templates
 Harbor::View.cache_templates!
 
-config.couchdb = "http://#{File.read(".couchdb").strip}@ssmoot.cloudant.com/blog"
+config.couchdb = "http://#{File.read(".couchdb").strip}@ssmoot.cloudant.com:5984/blog"
