1ac99fd7e92823abb999dc03a0be605a79b2ac09
Sam Smoot <ssmoot@gmail.com>
2012-08-09 14:32:09 -0500

    Added production connection-string.

--- env/production.rb
+++ env/production.rb
@@ -1,2 +1,4 @@
 # Make use of Tilt (https://github.com/rtomayko/tilt) cache for compiled templates
 Harbor::View.cache_templates!
+
+config.couchdb = "http://#{File.read(".couchdb")}@ssmoot.cloudant.com/blog"
