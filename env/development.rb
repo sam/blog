4b0b74544917d1ff302f13a848ef10de7fd1d544
Sam Smoot <ssmoot@gmail.com>
2012-08-10 15:46:02 -0500

    Just removing some test tuning from env/development.

--- env/development.rb
+++ env/development.rb
@@ -1,6 +1,4 @@
-# config.reloader.enable!
+config.reloader.enable!
 config.assets.compile = true
 
-Harbor::View.cache_templates!
-
-# Harbor.serve_public_files! config.root + 'public'
+Harbor.serve_public_files! config.root + 'public'
