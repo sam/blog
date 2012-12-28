b23ce6bb92a8f069c6450e847b7e82207c0877d4
Sam Smoot <ssmoot@gmail.com>
2012-12-28 16:15:21 -0600

    Created a helper Couch object for querying views.

--- app/controllers/Posts.scala
+++ app/controllers/Posts.scala
@@ -2,25 +2,11 @@ package controllers
 
 import play.api.mvc._
 import models._
-import play.api.libs.ws.WS
-import play.api.libs.concurrent.Akka
 
 object Posts extends Controller {
 
   def index = Action {
-    import play.api.Play.current
-    val akkaSystem = Akka.system
-    import akkaSystem.dispatcher
-
-    Async {
-      WS.url("http://127.0.0.1:5984/blog/_design/posts/_view/categories?group=true").get().map { response =>
-        val categories = (response.json \ "rows" \\ "key").map(_.as[String])
-
-        Ok(views.html.Posts.index(Post.recent, Post.archive, categories))
-      }
-    }
-
-
+    Ok(views.html.Posts.index(Post.recent, Post.archive, Category.titles))
   }
 
   def show(slug: String) = TODO
--- app/models/Category.scala
+++ app/models/Category.scala
@@ -1,14 +1,26 @@
 package models
 
+import play.api.libs.ws.{Response, WS}
+import play.api.libs.concurrent.Akka
+import akka.util.Timeout
+import concurrent.Await
+
 case class Category(title:String)
 
+object Couch {
+  def query[T](url:String, response:Response => T):T = {
+    import play.api.Play.current
+    val akkaSystem = Akka.system
+    import akkaSystem.dispatcher
+
+    response(Await.result(WS.url(url).get, Timeout(1000).duration))
+  }
+}
+
 object Category {
-
-  def titles = Seq("One", "Two", "Three")
-
-//  def titles
-//  @cache.get("categories") do
-//  COUCH.view("posts/categories", group: true)["rows"].map { |row| row["key"] }
-//  end
-//  end
+  def titles = {
+    Couch.query[Seq[String]]("http://127.0.0.1:5984/blog/_design/posts/_view/categories?group=true", {
+      response => (response.json \ "rows" \\ "key").map(_.as[String])
+    })
+  }
 }
