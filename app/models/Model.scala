c19e0c50277c067b063ef313054a5920c7f7e080
Sam Smoot <ssmoot@gmail.com>
2013-01-03 12:17:46 -0600

    Added usage of conf/couchdb.properties if present. closes #4

--- app/models/Model.scala
+++ app/models/Model.scala
@@ -1,26 +1,27 @@
 package models
 
 import concurrent.Future
-import spray.json.{JsValue, JsonFormat}
 
 trait Model {
   import play.api.libs.concurrent.Akka
   import sprouch._
 
-  import play.api.Play.current
+  import play.api.Play._
 
   val actorSystem = Akka.system
   implicit val dispatcher = (actorSystem.dispatcher)
 
-  val host = "ssmoot.cloudant.com"
-  val port = 5984
-  val userPass = Some("" -> "")
-  val https = false
+  val config = configuration.getConfig("couchdb").map { couchdb =>
+    Config(actorSystem,
+      couchdb.getString("host").getOrElse("localhost"),
+      couchdb.getInt("port").getOrElse(5984),
+      couchdb.getString("username").map(username => Some(username -> couchdb.getString("password").getOrElse(""))).getOrElse(None),
+      couchdb.getBoolean("https").getOrElse(false))
+  }.getOrElse(Config(actorSystem))
 
-  val config = Config(actorSystem, host, port, userPass, https)
   val couch = Couch(config)
 
-  val db = couch.getDb("blog")
+  val db = couch.getDb(configuration.getString("couchdb.database").getOrElse("blog"))
 
   def withDb[T](view:Database => Future[T]) = {
     db.flatMap(view)
