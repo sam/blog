3d6c7c5b1eef0e237867c25504dc12f3a8fd08bf
Sam Smoot <ssmoot@gmail.com>
2013-01-02 19:37:16 -0600

    Added caching to /posts action.

--- app/controllers/Posts.scala
+++ app/controllers/Posts.scala
@@ -2,18 +2,22 @@ package controllers
 
 import play.api.mvc._
 import models._
+import play.api.cache.Cached
 
 object Posts extends Controller with AkkaExecutionContext {
+  import play.api.Play.current
   import akkaSystem.dispatcher
 
-  def index = Action {
-    Async {
-      for {
-        titles <- Category.titles
-        recent <- Post.recent
-        archive <- Post.archive(recent.last.key)
+  def index = Cached("posts", 1000) {
+    Action {
+      Async {
+        for {
+          titles <- Category.titles
+          recent <- Post.recent
+          archive <- Post.archive(recent.last.key)
+        }
+        yield Ok(views.html.Posts.index(recent.map(_.value), archive.map(_.value), titles))
       }
-      yield Ok(views.html.Posts.index(recent.map(_.value), archive.map(_.value), titles))
     }
   }
 
