c2fdf97b666a4fc823eea203934736bcfb93c269
Sam Smoot <ssmoot@gmail.com>
2013-01-03 17:23:19 -0600

    Removed some boiler-plate from BCryptSpec.

--- test/helpers/BCryptSpec.scala
+++ test/helpers/BCryptSpec.scala
@@ -4,17 +4,12 @@ import org.specs2.mutable._
 
 import play.api.test._
 import play.api.test.Helpers._
+import helpers.BCrypt._
 
-/**
- * Add your spec here.
- * You can mock out a whole application including requests, plugins etc.
- * For more information, consult the wiki.
- */
 class BCryptSpec extends Specification {
 
   "BCrypt" should {
     "implicitly convert a String to a Password" in {
-      import helpers.BCrypt._
       "bob".bcrypt must beAnInstanceOf[Password]
     }
   }
