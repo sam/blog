174c505f787c76681a6d197be135ebc9ba1a9697
Sam Smoot <ssmoot@gmail.com>
2013-01-09 14:55:32 -0600

    getOrElse the last-modified value to build the cache key.

--- app/controllers/Posts.scala
+++ app/controllers/Posts.scala
@@ -10,7 +10,7 @@ object Posts extends Controller with AkkaExecutionContext {
   import play.api.Play.current
   import akkaSystem.dispatcher
 
-  def index = Cached(s"${Cache.get("modifiedAt")}-posts", 60) {
+  def index = Cached(s"${Cache.get("modifiedAt").getOrElse(0)}-posts", 60) {
     Action {
       Async {
         for {
@@ -23,7 +23,7 @@ object Posts extends Controller with AkkaExecutionContext {
     }
   }
 
-  def show(slug: String) = Cached(s"${Cache.get("modifiedAt")}-posts-$slug", 60) {
+  def show(slug: String) = Cached(s"${Cache.get("modifiedAt").getOrElse(0)}-posts-$slug", 60) {
     Action {
       Async {
         for {
