a0eb3d7710bec6f424d6cb7a1cc2421b80b85307
Richard Searle <harold.richard.searle@gmail.com>
2013-02-04 22:05:00 -0600

    fully async

--- app/controllers/admin/Posts.scala
+++ app/controllers/admin/Posts.scala
@@ -2,8 +2,8 @@ package controllers.admin
 
 import play.api.mvc._
 import models._
-import controllers.{admin, Secured}
-import concurrent.Await
+import controllers.{ admin, Secured }
+import concurrent.Future
 import play.api.cache.Cache
 import java.util.Date
 
@@ -12,47 +12,48 @@ object Posts extends Secured {
   import akkaSystem.dispatcher
   import concurrent.duration._
 
-  def index = withAuth { username => implicit request =>
-    Async {
-      for {
-        posts <- Post.all
-        categories <- Category.titles
+  def index = withAuth { username =>
+    implicit request =>
+      Async {
+        for {
+          posts <- Post.all
+          categories <- Category.titles
+        } yield Ok(views.html.admin.Posts.index(posts.docs[Post])(categories, flash))
       }
-      yield Ok(views.html.admin.Posts.index(posts.docs[Post])(categories, flash))
-    }
   }
 
-  def create = withAuth { username => implicit request =>
-    Async {
-      for(categories <- Category.titles)
-      yield postForm.bindFromRequest.fold(
-        errors => BadRequest(views.html.admin.Posts.edit(errors)(categories)),
-        post => {
-          Await.result(Post.create(post), 5 seconds)
-          Cache.set("modifiedAt", new Date().getTime)
-          Redirect(admin.routes.Posts.index).flashing("success" -> s"""Post "${post.title}" successfully CREATED.""")
-        }
-      )
-    }
-  }
-
-  def newPost = withAuth { username => implicit request =>
-    Async {
-      for(categories <- Category.titles)
-      yield Ok(views.html.admin.Posts.edit(postForm)(categories))
-    }
-  }
-
-  def edit(id:String) = withAuth { username => implicit request =>
-    Async {
-      for {
-        post <- Post.getById(id)
-        categories <- Category.titles
+  def create = withAuth { username =>
+    implicit request =>
+      Async {
+        postForm.bindFromRequest.fold(
+          errors => Category.titles map (categories => BadRequest(views.html.admin.Posts.edit(errors)(categories))),
+          post => {
+            Post.create(post) map { _ =>
+              Cache.set("modifiedAt", new Date().getTime)
+              Redirect(admin.routes.Posts.index).flashing("success" -> s"""Post "${post.title}" successfully CREATED.""")
+            }
+          })
       }
-      yield post.map { post =>
-        Ok(views.html.admin.Posts.edit(postForm.fill(post))(categories))
-      }.getOrElse(NotFound)
-    }
+  }
+
+  def newPost = withAuth { username =>
+    implicit request =>
+      Async {
+        for (categories <- Category.titles)
+          yield Ok(views.html.admin.Posts.edit(postForm)(categories))
+      }
+  }
+
+  def edit(id: String) = withAuth { username =>
+    implicit request =>
+      Async {
+        for {
+          post <- Post.getById(id)
+          categories <- Category.titles
+        } yield post.map { post =>
+          Ok(views.html.admin.Posts.edit(postForm.fill(post))(categories))
+        }.getOrElse(NotFound)
+      }
   }
 
   def preview = Action { implicit request =>
@@ -62,28 +63,28 @@ object Posts extends Secured {
     }.getOrElse(BadRequest("Preview requires an encoded Form"))
   }
 
-  def update(id:String) = withAuth { username => implicit request =>
-    Async {
-      for(categories <- Category.titles)
-      yield postForm.bindFromRequest.fold(
-        errors => BadRequest(views.html.admin.Posts.edit(errors)(categories)),
-        post => {
-          Await.result(Post.update(id, post), 5 seconds)
-          Cache.set("modifiedAt", new Date().getTime)
-          Redirect(admin.routes.Posts.index).flashing("success" -> s"""Post "${post.title}" successfully UPDATED.""")
-        }
-      )
-    }
+  def update(id: String) = withAuth { username =>
+    implicit request =>
+      Async {
+        postForm.bindFromRequest.fold(
+          errors => Category.titles map (categories => BadRequest(views.html.admin.Posts.edit(errors)(categories))),
+          post => {
+            Post.update(id, post).map { _ =>
+              Cache.set("modifiedAt", new Date().getTime)
+              Redirect(admin.routes.Posts.index).flashing("success" -> s"""Post "${post.title}" successfully UPDATED.""")
+            }
+          })
+      }
   }
 
-  def delete(id: String) = withAuth { username => implicit request =>
-    Async {
-      for((_, post) <- Post.delete(id))
-      yield {
-        Cache.set("modifiedAt", new Date().getTime)
-        Redirect(routes.Posts.index).flashing("success" -> s"""Post "${post.data.title}" successfully DELETED.""")
+  def delete(id: String) = withAuth { username =>
+    implicit request =>
+      Async {
+        for ((_, post) <- Post.delete(id)) yield {
+          Cache.set("modifiedAt", new Date().getTime)
+          Redirect(routes.Posts.index).flashing("success" -> s"""Post "${post.data.title}" successfully DELETED.""")
+        }
       }
-    }
   }
 
   import play.api.data.Form
@@ -95,9 +96,5 @@ object Posts extends Secured {
       "slug" -> nonEmptyText,
       "publishedAt" -> optional(date("yyyy-MM-dd")),
       "body" -> optional(text),
-      "categories" -> list(text)
-    )
-      ((title, slug, publishedAt, body, categories) => Post(None, title, slug, publishedAt, body, Some(categories)))
-      ((post: Post) => Some((post.title, post.slug, post.publishedAt, post.body, post.categories.getOrElse(Nil).toList)))
-  )
+      "categories" -> list(text))((title, slug, publishedAt, body, categories) => Post(None, title, slug, publishedAt, body, Some(categories)))((post: Post) => Some((post.title, post.slug, post.publishedAt, post.body, post.categories.getOrElse(Nil).toList))))
 }
